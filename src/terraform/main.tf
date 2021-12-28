01151af3c745d4dc98e671450fff28688890a3d0
Chuck D'Antonio <cdantonio@pivotal.io>
2021-12-28 18:05:00 +0000

    No longer attempts to create a content library

--- src/terraform/main.tf
+++
@@ -1,9 +0,0 @@
-data "vsphere_content_library" "library" {
-  name = var.vsphere_content_library
-}
-
-resource "vsphere_content_library_item" "template" {
-  name        = var.template_name
-  description = "Windows template with OS 11 instsalled. Use for OutSystems platform, front end, and LifeTime servers"
-  library_id  = data.vsphere_content_library.library.id
-}
